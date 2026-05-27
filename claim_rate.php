5f8189b327866b32af2bfec7918b7b45f4771e9b
Magic autosync <autosync@magic.local>
2026-05-27 19:20:03 +0000

    Auto sync: claim_rate.php (2026-05-27 19:20)

--- claim_rate.php
+++ claim_rate.php
@@ -2,5 +2,5 @@
 // Auto-generated. IP => unix-timestamp of last successful claim.
 defined('DASHBOARD') or exit;
 return array (
-  '162.159.102.11' => 1779906868,
+  '162.159.102.12' => 1779909537,
 );
